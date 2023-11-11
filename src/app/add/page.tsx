a3ecc4c0f72393563d2e64ce5bbc3b27158f1391
makileth <focyfox@gmail.com>
2023-11-11 14:18:38 +0100

    visual fixes for add page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -20,7 +20,6 @@ type Input = {
   catSlug: string;
 };
 const AddPage = () => {
-
   const { data: session, status } = useSession();
 
   const [file, setFile] = useState<File>(); // image
@@ -70,10 +69,7 @@ const AddPage = () => {
     console.log(options); // for debugging
   };
 
-
-
   const handleChangeImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-
     // we access the HTML-Element that activated the event
     const target = e.target as HTMLInputElement;
 
@@ -82,22 +78,23 @@ const AddPage = () => {
 
     // lets save the file
     setFile(item);
-
   };
 
   const upload = async () => {
-
     const data = new FormData(); // by using this we will collect our img and some other info for our http-request
 
     data.append("file", file!); // we give the file over to data
 
     data.append("upload_preset", "food-app"); // add our preset from Cloudinary
 
-    const res = await fetch("https://api.cloudinary.com/v1_1/dn2ep2prg/image/upload", {
-      method: "POST",
-    //  headers: { "Content-Type": "multipart/form-data" },
-      body: data,
-    });
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dn2ep2prg/image/upload",
+      {
+        method: "POST",
+        //  headers: { "Content-Type": "multipart/form-data" },
+        body: data,
+      }
+    );
 
     const resData = await res.json(); // the answer from the server
 
@@ -107,19 +104,22 @@ const AddPage = () => {
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-
     e.preventDefault();
 
     try {
       const url = await upload();
-      const res = await fetch("https://restaurant-app-dusky.vercel.app/api/products", { // https://restaurant-app-dusky.vercel.app
-        method: "POST",
-        body: JSON.stringify({
-          img: url,
-          ...inputs, // everything inside inputs
-          options,
-        }),
-      });
+      const res = await fetch(
+        "https://restaurant-app-dusky.vercel.app/api/products",
+        {
+          // https://restaurant-app-dusky.vercel.app
+          method: "POST",
+          body: JSON.stringify({
+            img: url,
+            ...inputs, // everything inside inputs
+            options,
+          }),
+        }
+      );
 
       const data = await res.json();
 
@@ -139,157 +139,165 @@ const AddPage = () => {
   };
 
   return (
-    <form
-      action=""
-      className=" mt-[5rem] shadow-md rounded-[10px] px-8 pt-6 pb-8 mb-4"
-      onSubmit={handleSubmit}
-    >
-      <h1 className="block text-sunshineYellow text-[24px] font-bold mb-2">
-        Add a new product
-      </h1>
-      <div className="mb-4">
-        <label
-          className="block text-white text-sm font-bold mb-2"
-          htmlFor="productName"
-        >
-          Product Name
-        </label>
-        <input
-          className="shadow bg-neutral-800 appearance-none border rounded-[10px] w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Product's name"
-          id="title"
-          name="title"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="mb-4">
-        <label
-          className="block text-white text-sm font-bold mb-2"
-          htmlFor="productName"
-        >
-          Product Image
-        </label>
-        {/* {imageUrl && <Image src={imageUrl} width={24} height={24} alt="" />} */}
-        <input type="file" onChange={handleChangeImage} className="bg-neutral-800 text-white  rounded-[10px] p-2" />
-      </div>
-
-      <div className="mb-4">
-        <label
-          className="block text-white text-sm font-bold mb-2"
-          htmlFor="category"
-        >
-          Category
-        </label>
-        <input
-          className="shadow appearance-none border rounded-[10px] bg-neutral-800 w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Category"
-          id="category"
-          name="catSlug"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="mb-4 ">
-        <label
-          className="block text-white text-sm font-bold mb-2"
-          htmlFor="description"
-        >
-          Description
-        </label>
-        <textarea
-          className="shadow appearance-none bg-neutral-800 border rounded-[10px] w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-          name="desc"
-          id="description"
-          cols={30}
-          rows={10}
-          onChange={handleChange}
-        ></textarea>
-      </div>
-      <div className="mb-4">
-        <label
-          className="block text-white text-sm font-bold mb-2"
-          htmlFor="price"
-        >
-          Price
-        </label>
-        <input
-          className="shadow appearance-none border rounded-[10px] w-full bg-neutral-800 py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="123.00$"
-          id="price"
-          name="price"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="mb-4">
-        <label
-          className="block text-white text-sm font-bold mb-2"
-          htmlFor="inputs"
-        >
-          Options
-        </label>
-        <div className="space-y-4 mb-4 ">
+    <section className="flex items-center justify-center mx-auto max-w-4xl min-h-[100vh]">
+      <form
+        action=""
+        className=" mt-[5rem] shadow-md rounded-[10px] px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="block text-sunshineYellow text-[24px] font-bold mb-2">
+          Add a new product
+        </h1>
+        <div className="mb-4">
+          <label
+            className="block text-white text-sm font-bold mb-2"
+            htmlFor="productName"
+          >
+            Product Name
+          </label>
           <input
-            onChange={handleChangeOption}
-            className="shadow appearance-none border bg-neutral-800 rounded-[10px] w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow bg-neutral-800 appearance-none border rounded-[10px] w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
             type="text"
-            placeholder="Title"
+            placeholder="Product's name"
+            id="title"
             name="title"
-            id="inputs"
+            onChange={handleChange}
           />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-white text-sm font-bold mb-2"
+            htmlFor="productName"
+          >
+            Product Image
+          </label>
+          {/* {imageUrl && <Image src={imageUrl} width={24} height={24} alt="" />} */}
           <input
-            onChange={handleChangeOption}
-            className="shadow appearance-none border bg-neutral-800 rounded-[10px] w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder="Additional Price"
-            name="additionalPrice"
+            type="file"
+            onChange={handleChangeImage}
+            className="bg-neutral-800 text-white  rounded-[10px] p-2"
           />
         </div>
-        <button
-          className="bg-neutral-900 hover:bg-black text-white font-bold py-2 px-4 rounded-[10px] focus:outline-none focus:shadow-outline mt-3"
-          type="button"
-          onClick={() => setOptions((prev) => [...prev, option])}
-        >
-          Add Option
-        </button>
-        <div>
-          {options.map((opt) => (
-            // use title because no id
-            <div key={opt.title} className="flex flex-row">
-              <div className="bg-white border-[3px] px-3 py-2 mt-4 font-semibold text-neutral-800 border-red-500 rounded-[10px]-[10px]">
-                <h4>
-                  {opt.title} ({opt.additionalPrice + "$"})
-                </h4>
-              </div>
-              <button
-                className="text-red-500 text-[21px] mt-4 mx-3"
-                onClick={() =>
-                  setOptions(options.filter((item) => item.title !== opt.title))
-                }
-              >
-                X
-              </button>
-            </div>
-          ))}
+
+        <div className="mb-4">
+          <label
+            className="block text-white text-sm font-bold mb-2"
+            htmlFor="category"
+          >
+            Category
+          </label>
+          <input
+            className="shadow appearance-none border rounded-[10px] bg-neutral-800 w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Category"
+            id="category"
+            name="catSlug"
+            onChange={handleChange}
+          />
         </div>
-      </div>
-      <div className="mt-12 space-x-4">
-        <button
-          className="bg-sunshineYellow hover:shadow-2xl hover:shadow-sunshineYellow text-white font-bold py-2 px-4 rounded-[10px] focus:outline-none focus:shadow-outline"
-          type="submit"
-        >
-          Add
-        </button>
-        <button
-          className="bg-neutral-700 hover:bg-black text-white font-bold py-2 px-4 rounded-[10px] focus:outline-none focus:shadow-outline"
-          type="button"
-          onClick={() => router.push("/")}
-        >
-          Cancel
-        </button>
-      </div>
-    </form>
+        <div className="mb-4 ">
+          <label
+            className="block text-white text-sm font-bold mb-2"
+            htmlFor="description"
+          >
+            Description
+          </label>
+          <textarea
+            className="shadow appearance-none bg-neutral-800 border rounded-[10px] w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+            name="desc"
+            id="description"
+            cols={30}
+            rows={10}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-white text-sm font-bold mb-2"
+            htmlFor="price"
+          >
+            Price
+          </label>
+          <input
+            className="shadow appearance-none border rounded-[10px] w-full bg-neutral-800 py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="123.00$"
+            id="price"
+            name="price"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-white text-sm font-bold mb-2"
+            htmlFor="inputs"
+          >
+            Options
+          </label>
+          <div className="space-y-4 mb-4 ">
+            <input
+              onChange={handleChangeOption}
+              className="shadow appearance-none border bg-neutral-800 rounded-[10px] w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Title"
+              name="title"
+              id="inputs"
+            />
+            <input
+              onChange={handleChangeOption}
+              className="shadow appearance-none border bg-neutral-800 rounded-[10px] w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Additional Price"
+              name="additionalPrice"
+            />
+          </div>
+          <button
+            className="bg-neutral-900 hover:bg-black text-white font-bold py-2 px-4 rounded-[10px] focus:outline-none focus:shadow-outline mt-3"
+            type="button"
+            onClick={() => setOptions((prev) => [...prev, option])}
+          >
+            Add Option
+          </button>
+          <div>
+            {options.map((opt) => (
+              // use title because no id
+              <div key={opt.title} className="flex flex-row">
+                <div className="bg-white border-[3px] px-3 py-2 mt-4 font-semibold text-neutral-800 border-red-500 rounded-[10px]-[10px]">
+                  <h4>
+                    {opt.title} ({opt.additionalPrice + "$"})
+                  </h4>
+                </div>
+                <button
+                  className="text-red-500 text-[21px] mt-4 mx-3"
+                  onClick={() =>
+                    setOptions(
+                      options.filter((item) => item.title !== opt.title)
+                    )
+                  }
+                >
+                  X
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="mt-12 space-x-4">
+          <button
+            className="bg-sunshineYellow hover:shadow-2xl hover:shadow-sunshineYellow text-white font-bold py-2 px-4 rounded-[10px] focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Add
+          </button>
+          <button
+            className="bg-neutral-700 hover:bg-black text-white font-bold py-2 px-4 rounded-[10px] focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={() => router.push("/")}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
